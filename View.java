c5bc6cd29eba87ba33e8d6312c65fdea7f802004
SuperRandomName <48484291+SuperRandomName@users.noreply.github.com>
2019-03-12 16:56:21 +0100

    Update View.java

--- View.java
+++ View.java
@@ -1,1 +1,24 @@
+package mindel;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class View implements Observer {
+	
+	protected State state;
+	
+	public View(State state) {
+		
+		this.state = state;
+		
+	}
+	
+
+	
+	public void update(Observable arg0, Object arg1) {
+
+		
+	}
+
+}
 
