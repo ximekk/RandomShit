f2a847ca7e7a744efd067a295368705bffdff159
SuperRandomName <48484291+SuperRandomName@users.noreply.github.com>
2019-03-12 18:09:42 +0100

    Update View.java

--- View.java
+++ View.java
@@ -1,19 +1,32 @@
+//SKA ABSTRAKTA METODER ANVÄNDAS??
+
 package mindel;
 
 import java.util.Observable;
 import java.util.Observer;
 
+/**
+ * @author Mikael Granström, Erik Olausson, Sermed Mutter, Amir Rakshan
+ *
+ */
+
+
 public class View implements Observer {
 	
 	protected State state;
 	
+	
+	/**
+ 	* @param state Simuleringens tillstånd.
+ 	*/
+	
 	public View(State state) {
-		
-		this.state = state;
-		
+		this.state = state;	
 	}
 	
-
+	/**
+ 	* Metod som uppdaterar vyn m.h.a Observer.
+ 	*/
 	
 	public void update(Observable arg0, Object arg1) {
 
